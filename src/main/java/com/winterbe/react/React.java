a77d4ccfbe747fac16e9dc8a6d79d70697a6e897
Jirka Marsik <jiri.marsik@oracle.com>
2019-10-30 12:11:07 +0100

    Port the app to use the Graal SDK

--- src/main/java/com/winterbe/react/React.java
+++ src/main/java/com/winterbe/react/React.java
@@ -1,9 +1,11 @@
 package com.winterbe.react;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotException;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -11,25 +13,23 @@ import java.util.List;
 
 public class React {
 
-    private ThreadLocal<ScriptEngine> engineHolder = new ThreadLocal<ScriptEngine>() {
-        @Override
-        protected ScriptEngine initialValue() {
-            ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("Graal.js");
+    private ThreadLocal<Context> contextHolder = ThreadLocal.withInitial(() -> {
+            Context context = Context.newBuilder("js").allowHostAccess(true).allowExperimentalOptions(true).option("js.nashorn-compat", "true").build();
             try {
-                scriptEngine.eval(read("static/vendor/react.js"));
-                scriptEngine.eval(read("static/vendor/showdown.min.js"));
-                scriptEngine.eval(read("static/commentBox.js"));
-            } catch (ScriptException e) {
+                for (String path : new String[]{"static/vendor/react.js", "static/vendor/showdown.min.js", "static/commentBox.js"}) {
+                    Source src = Source.newBuilder("js", read(path), path).build();
+                    context.eval(src);
+                }
+            } catch (PolyglotException | IOException e) {
                 throw new RuntimeException(e);
             }
-            return scriptEngine;
-        }
-    };
+            return context;
+    });
 
-    public  String renderCommentBox(List<Comment> comments) {
+    public String renderCommentBox(List<Comment> comments) {
         try {
-            Object html = ((Invocable)engineHolder.get()).invokeFunction("renderServer", comments);
-            return String.valueOf(html);
+            Value html = contextHolder.get().getBindings("js").getMember("renderServer").execute(comments);
+            return html.toString();
         }
         catch (Exception e) {
             throw new IllegalStateException("failed to render react component", e);
